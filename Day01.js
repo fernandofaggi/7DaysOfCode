6cb607b97b826c037bbd711af1b388ac881d9af0
Fernando Faggi <68256665+fernandofaggi@users.noreply.github.com>
2022-07-05 15:47:30 -0300

    Add files via upload
    Aula 01

---
+++ Day01.js
@@ -0,0 +1,36 @@
+
+
+/*Sendo assim, a sua tarefa de hoje é reescrever 
+o código abaixo de maneira que ele imprima as informações 
+de maneira correta, que faça sentido e sem erros:
+-----------------------------------------------------------------*/
+
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+
+//COMPARAR O numeroUm e a stringUm
+if (numeroUm == stringUm ) {
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} 
+else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+// COMPARA numero 30 e string 30
+if (numeroTrinta == stringTrinta ) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+// compara numero 10 e string 10 
+if (numeroDez == stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
+
