a6dff6d62e1914c693c5eca7dccb5531a6854cc3
fernando <fernandofaggi@gmail.com>
2022-07-05 16:04:12 -0300

    Correcao num30 e pula linha

--- Day01.js
+++ Day01.js
@@ -15,22 +15,22 @@ let stringDez = '10'
 
 //COMPARAR O numeroUm e a stringUm
 if (numeroUm == stringUm ) {
-  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes', "\n")
 } 
 else {
-  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor', "\n")
 }
 
 // COMPARA numero 30 e string 30
-if (numeroTrinta == stringTrinta ) {
-  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+if (numeroTrinta === stringTrinta) {
+  console.log( 'As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo', "\n" )
 } else {
-  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo', "\n")
 }
 // compara numero 10 e string 10 
 if (numeroDez == stringDez) {
-  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes', "\n")
 } else {
-  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor', "\n")
 }
 
