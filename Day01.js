c23e9f7c12f38dc67c629fa0b9d60de23d50a0d5
fernando <fernandofaggi@gmail.com>
2022-07-06 22:25:13 -0300

    Day02

--- Day01.js
+++ Day01.js
@@ -8,7 +8,7 @@ de maneira correta, que faça sentido e sem erros:
 let numeroUm = 1
 let stringUm = '1'
 let numeroTrinta = 30
-let stringTrinta = '30'
+let stringTrinta = '40'
 let numeroDez = 10
 let stringDez = '10'
 
